d6017d1f3162d81106f82d70cf1a41ba1c9d6400
TieCoders <a.hannachi@tiecoders.com>
2018-11-13 23:38:16 +0100

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PokemonCS.Startup))]
+namespace PokemonCS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
